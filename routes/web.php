a4fea56eb8350160b271eccd0eb37fa5bfa4ea10
Alvian <alvian.ardhiansyah@gmail.com>
2020-04-16 23:08:50 +0700

    revisi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,15 @@ Route::get('/coba', function(){
     return view('auth.login1');
 });
 
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin',
+                'middelware'=>'Auth'
+            ], function () {
+                Route::get('/home', 'HomeController@index')->name('home');
+
+                Route::resource('category','CategoryController');
+            });
+
+
